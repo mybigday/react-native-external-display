61d4344d5913df8761da62c84e80300470a83782
Jhen <developer@jhen.me>
2023-06-19 13:09:02 +0800

    chore: update deps

--- packages/RNExternalDisplayExample/index.js
+++ packages/RNExternalDisplayExample/index.js
@@ -3,11 +3,8 @@
  */
 
 import { AppRegistry } from 'react-native'
-import { enableScreens } from 'react-native-screens'
 
 import App from './js/App'
 import { name as appName } from './app.json'
-
-enableScreens()
 
 AppRegistry.registerComponent(appName, () => App)
